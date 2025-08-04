626fea38cd439fec0353b8842f2e3f12a360930e
USSNIPER <Liamnoy4@gmail.com>
2025-08-04 22:28:57 +0300

    Delete Cartel Cheats Web directory

--- Cartel Cheats Web/js/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAyC6mxNHZ_lNNZXb6YngZdFFf3d6LdIWQ",
-    authDomain: "cartel-cheats.firebaseapp.com",
-    projectId: "cartel-cheats",
-    storageBucket: "cartel-cheats.firebasestorage.app",
-    messagingSenderId: "290099986212",
-    appId: "1:290099986212:web:f5a155d59f083d47cb3db1",
-    measurementId: "G-FEB19T0SJE"
-};
